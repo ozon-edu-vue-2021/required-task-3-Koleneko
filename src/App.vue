7f19f1d914ecca50efbe6d3fb6ffb1aeacb7b6bc
Koleneko <vkoleneko@gmail.com>
2021-11-03 23:03:42 +0300

    Completed assignment

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map @clicked="tableClicked" />
+      <SideMenu
+        @update:isUserOpenned="profileClosed"
+        :isUserOpenned="isUserOpenned"
+        :choosenUserId="choosenUserId"
+      />
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,44 +17,59 @@ import SideMenu from "./components/SideMenu.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      isUserOpenned: false,
+      choosenUserId: null,
+    };
+  },
   components: {
     Map,
     SideMenu,
+  },
+  methods: {
+    tableClicked(id) {
+      this.choosenUserId = id;
+      this.isUserOpenned = true;
+    },
+    profileClosed() {
+      this.isUserOpenned = false;
+    },
   },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
